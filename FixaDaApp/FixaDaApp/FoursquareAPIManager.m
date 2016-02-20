587811e3793a522a3139c7de02718d17150f5472
zoufishanmehdi <zzmehdi@gmail.com>
2016-02-20 16:29:26 -0500

    I think I'm done

--- FixaDaApp/FixaDaApp/FoursquareAPIManager.m
+++ FixaDaApp/FixaDaApp/FoursquareAPIManager.m
@@ -12,6 +12,7 @@
 #define kFoursquareAPIClientID     @"GWKJBVWFYBJQ02T3TRBB4VBL24AIO4TCMJCGIQ5ADKVKJXGP"
 #define kFoursquareAPIClientSecret @"2WMEZCDQNKNB5XAE5F4BY1VHBK1HITYRU1JEVCOAD2QRLXDJ"
 
+
 @implementation FoursquareAPIManager
 
 /**
@@ -29,6 +30,8 @@
            atLocation:(CLLocation *)location
            completion:(void(^)(NSArray *data))completion
 {
+    #define query @"restaurant"
+    
     NSString *baseURL = @"https://api.foursquare.com/v2/venues/search";
     NSString *url = [NSString stringWithFormat:@"%@?client_id=%@&client_secret=%@&v=20160215&ll=%f,%f&query=%@", baseURL, kFoursquareAPIClientID, kFoursquareAPIClientSecret, location.coordinate.latitude, location.coordinate.longitude, query];
     
@@ -39,6 +42,30 @@
         progress:nil
          success:^(NSURLSessionTask *task, id responseObject)
     {
+        
+        NSDictionary *results = responseObject[@"response"];
+        NSArray *data = results[@"venues"];
+        NSLog(@"%@", results);
+        completion(data);
+        
+//         for (NSDictionary *result in docs) {
+//             
+//             NSString *venueName = [result objectForKey:@"name"];
+//              NSLog(@"%@", venueName);
+//              NSDictionary *venueLocation = [result objectForKey:@"location"];
+//             double latitude = [[venueLocation objectForKey:@"lat"] doubleValue];
+//              NSLog(@"%f", latitude);
+//             double longitude = [[venueLocation objectForKey:@"lng"] doubleValue];
+//              NSLog(@"%f", longitude);
+//             
+//             CLLocationCoordinate2D location = CLLocationCoordinate2DMake(latitude, longitude);
+            ////*****the code below is not needed*****////
+            // completion(location);
+             //        [self.tableView reloadData];
+             
+             
+            // NSLog(@"response object: %@", responseObject);
+//         }
         
     } failure:^(NSURLSessionTask *operation, NSError *error)
     {
